694f43bc33133bf468374034eb408ba603f2aa75
tirtza <t7678026@gmail.com>
2019-04-28 14:36:09 +0300

    router
    router

--- Angular/Location/src/app/app-routing.module.ts
+++ Angular/Location/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditLocationComponent } from './edit-location/edit-location.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'home',
+  component:EditLocationComponent
+
+},
+{path:'',
+redirectTo: '/home',
+pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
